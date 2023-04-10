ed8dec0e98f7a0cc78900fe327f32fdfd7781e4a
GEEK1050 <karim-bahri-18@outlook.fr>
2023-04-10 02:42:29 +0100

    DELETE_USER & UPDATE_USER_PASSWORD implementation

--- server/src/schema/mutations/user.ts
+++ server/src/schema/mutations/user.ts
@@ -1,8 +1,8 @@
-import { GraphQLString } from "graphql";
+import { GraphQLID, GraphQLString } from "graphql";
 import UserType from "../typedefs/user";
 import { Users } from "../../entities";
 
-const CREATE_USER = {
+export const CREATE_USER = {
   type: UserType,
   args: {
     name: { type: GraphQLString },
@@ -20,4 +20,41 @@ const CREATE_USER = {
   },
 };
 
-export default CREATE_USER;
+export const DELETE_USER = {
+  type: UserType,
+  args: {
+    id: { type: GraphQLID },
+  },
+  async resolve(parent: any, args: any) {
+    try {
+      const { id } = args;
+      await Users.delete(id);
+    } catch (err) {
+      console.log("ERROR : ", err);
+    }
+  },
+};
+
+export const UPDATE_USER_PASSWORD = {
+  type: UserType,
+  args: {
+    userName: GraphQLString,
+    oldPassword: GraphQLString,
+    newPassword: GraphQLString,
+  },
+  async resolve(parent: any, args: any) {
+    try {
+      const { userName, oldPassword, newPassword } = args;
+
+      const user = await Users.findOne({ where: { userName: userName } });
+      const userPassword = user?.password;
+      if (oldPassword === userPassword) {
+        await Users.update({ userName: userName }, { password: newPassword });
+      } else {
+        throw new Error("Password doesn't match");
+      }
+    } catch (err) {
+      console.log("ERROR : ", err);
+    }
+  },
+};
