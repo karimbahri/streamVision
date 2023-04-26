382352b8e976686c20851a81f0ea06c20de0be15
GEEK1050 <karim-bahri-18@outlook.fr>
2023-04-26 23:36:28 +0100

    update schema : adding login mutation

--- server/src/schema/index.ts
+++ server/src/schema/index.ts
@@ -1,5 +1,11 @@
 import { GraphQLObjectType, GraphQLSchema } from "graphql";
-import CREATE_USER from "./mutations/user";
+import {
+  CREATE_USER,
+  DELETE_USER,
+  LOGIN,
+  UPDATE_USER,
+  UPDATE_USER_PASSWORD,
+} from "./mutations/user";
 import GET_ALL_USERS from "./queries/user";
 
 const rootQuery = new GraphQLObjectType({
@@ -13,6 +19,10 @@ const mutation = new GraphQLObjectType({
   name: "Mutation",
   fields: {
     createUser: CREATE_USER,
+    login: LOGIN,
+    deleteUser: DELETE_USER,
+    updateUserPassword: UPDATE_USER_PASSWORD,
+    updateUser: UPDATE_USER,
   },
 });
 
