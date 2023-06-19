4bc1ea816b026e69e25f08cbe1a0b2f035af35c5
GEEK1050 <karim-bahri-18@outlook.fr>
2023-06-19 16:27:26 +0100

    updating streamPage

---
+++ client/src/components/streamPage.tsx
@@ -0,0 +1,23 @@
+import EpisodesList from "./episodesList";
+import StreamPlayer from "./streamPlayer";
+import { isLoggedIn } from "../utils";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useEffect } from "react";
+
+export default function StreamPage() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const videoId = searchParams.get("video");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoggedIn()) navigate("/login");
+  }, []);
+
+  return (
+    <div className="stream-page">
+      <h1 className="stream-page__header">Interstellar</h1>
+      <StreamPlayer />
+      <EpisodesList />
+    </div>
+  );
+}
