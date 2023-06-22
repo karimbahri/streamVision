06650b3190856f6fd53f672fb2db004c79cf8e3e
GEEK1050 <karim-bahri-18@outlook.fr>
2023-06-22 01:55:50 +0100

    set notification component

--- client/src/components/notification.tsx
+++ client/src/components/notification.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+
 export default function Notification(props: any) {
+  const [notificationClassValue, setNotificationClassValue] = useState("");
+  const [notificationValue, setNotificationValue] = useState("");
+
+  useEffect(() => {
+    setNotificationClassValue(props.classValue);
+    setNotificationValue(props.message);
+
+    setTimeout(() => {
+      setNotificationClassValue("");
+    }, 3000);
+  }, [props.classValue, props.message]);
   return (
-    <div className={`notification ${props.classValue}`}>
-      <p>{props.message}</p>
+    <div className={`notification ${notificationClassValue}`}>
+      <p>{notificationValue}</p>
     </div>
   );
 }
