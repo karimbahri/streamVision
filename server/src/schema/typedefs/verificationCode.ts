9931a14a429ac6b0a4ca0eb5d3d7347d35399112
GEEK1050 <karim-bahri-18@outlook.fr>
2023-08-30 04:50:42 +0100

    add verificationCode type

---
+++ server/src/schema/typedefs/verificationCode.ts
@@ -0,0 +1,12 @@
+import { GraphQLObjectType, GraphQLID, GraphQLString } from "graphql";
+
+const VerificationCodeType = new GraphQLObjectType({
+  name: "VerificationCode",
+  fields: () => ({
+    id: { type: GraphQLID },
+    email: { type: GraphQLString },
+    code: { type: GraphQLString },
+  }),
+});
+
+export default VerificationCodeType;
