c2b8df11135657051a92b51ead39789bd4ade79a
GEEK1050 <karim-bahri-18@outlook.fr>
2023-09-04 05:30:07 +0100

    implement resetPassword component

--- client/src/components/resetPassword.tsx
+++ client/src/components/resetPassword.tsx
@@ -1,14 +1,50 @@
 import * as Icon from "react-bootstrap-icons";
 import PasswordInput from "./passwordInput";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { setNotification } from "../utils";
 import Notification from "./notification";
+import { useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import client from "../apollo-client";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setCode,
+  setEmail as redux_setEmail,
+  setReset,
+} from "../redux/actions";
+
+const RESET_PASSWORD = gql`
+  mutation updateUserPassword(
+    $email: String!
+    $code: String!
+    $newPassword: String!
+    $oldPassword: String!
+  ) {
+    updateUserPassword(
+      email: $email
+      code: $code
+      newPassword: $newPassword
+      oldPassword: $oldPassword
+    ) {
+      id
+    }
+  }
+`;
 
 export default function ResetPassword() {
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
   const [notificationClassValue, setNotificationClassValue] = useState("");
   const [notificationValue, setNotificationValue] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [redux_code, setRedux_code] = useState(
+    useSelector((state: { code: any }) => state.code)
+  );
+  const [email, setEmail] = useState(
+    useSelector((state: { email: any }) => state.email)
+  );
 
   const setNotificationArgs = {
     notificationClassValue,
@@ -16,6 +52,41 @@ export default function ResetPassword() {
     notificationValue,
     setNotificationValue,
   };
+
+  const [updateUserPassword, { loading }] = useMutation(RESET_PASSWORD, {
+    onCompleted: () => {
+      client.resetStore();
+      dispatch(setCode(""));
+      dispatch(redux_setEmail(""));
+      setNotificationArgs.notificationClassValue =
+        "notification-appear notification-success";
+      setNotificationArgs.notificationValue = "Success !";
+      setNotification(setNotificationArgs);
+      navigate("/");
+    },
+    onError: (error) => {
+      setNotificationArgs.notificationClassValue =
+        "notification-appear notification-failure";
+      setNotificationArgs.notificationValue = error.message;
+      setNotification(setNotificationArgs);
+    },
+  });
+
+  useEffect(() => {
+    if (!redux_code) navigate("/");
+    if (loading) {
+      setNotificationArgs.notificationClassValue =
+        "notification-appear notification-success";
+      setNotificationArgs.notificationValue = "Loading...";
+      setNotification(setNotificationArgs);
+    }
+
+    return () => {
+      if (!redux_code) navigate("/");
+      dispatch(setReset(false));
+    };
+  }, [loading]);
+
   return (
     <div className="login">
       <h1 className="login__header">Reset password</h1>
@@ -40,7 +111,15 @@ export default function ResetPassword() {
               setNotificationArgs.notificationValue =
                 "Please fill all the fields";
               setNotification(setNotificationArgs);
-            } /*else await login({ variables: { email, password } });*/
+            } else
+              await updateUserPassword({
+                variables: {
+                  email,
+                  code: redux_code,
+                  oldPassword: password,
+                  newPassword: passwordConfirmation,
+                },
+              });
           }}
         >
           Save
