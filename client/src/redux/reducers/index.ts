451b45f1460ca15cffefb9fbcd68094c0c6affb7
GEEK1050 <karim-bahri-18@outlook.fr>
2023-09-05 22:47:09 +0100

    update combine reducer (adding timeoutid)

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import isResetReducer from "./isResetReducer";
 import emailReducer from "./emailReducer";
 import codeReducer from "./codeReducer";
+import timeoutIdReducer from "./timeoutIdReducer";
 
 const reducers = combineReducers({
   isReset: isResetReducer,
   email: emailReducer,
   code: codeReducer,
+  timeoutId: timeoutIdReducer,
 });
 
 export default reducers;
