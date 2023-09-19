6de94364310d87c55d70871c2c199ea3397b3fc1
GEEK1050 <karim-bahri-18@outlook.fr>
2023-09-19 21:29:05 +0100

    update show mutation (adding new attributes)

--- server/src/schema/mutations/shows.ts
+++ server/src/schema/mutations/shows.ts
@@ -1,21 +1,29 @@
-import { GraphQLBoolean, GraphQLID, GraphQLString } from "graphql";
+import { GraphQLInt, GraphQLString } from "graphql";
 import { Shows } from "../../entities";
 import ShowType from "../typedefs/shows";
 
-export const SAVE_MOVIEs = {
+export const SAVE_MOVIES = {
   type: ShowType,
   args: {
     content: { type: GraphQLString },
     title: { type: GraphQLString },
     thumbnail: { type: GraphQLString },
+    category: { type: GraphQLString },
+    seasons: { type: GraphQLInt },
+    episodes: { type: GraphQLInt },
   },
   async resolve(parent: any, args: any) {
-    let { content, title, thumbnail } = args;
+    let { content, title, thumbnail, category, seasons, episodes } = args;
+
+    title = title[0].toUpperCase() + title.slice(1).toLowerCase();
 
     await Shows.insert({
       content,
       title,
       thumbnail,
+      category,
+      seasons,
+      episodes,
     });
     return args;
   },
