0fee03ef627c556b081837b98c8a23227472d887
GEEK1050 <karim-bahri-18@outlook.fr>
2023-09-19 12:18:45 +0100

    init shows mutation

---
+++ server/src/schema/mutations/shows.ts
@@ -0,0 +1,22 @@
+import { GraphQLBoolean, GraphQLID, GraphQLString } from "graphql";
+import { Shows } from "../../entities";
+import ShowType from "../typedefs/shows";
+
+export const CREATE_USER = {
+  type: ShowType,
+  args: {
+    content: { type: GraphQLString },
+    title: { type: GraphQLString },
+    thumbnail: { type: GraphQLString },
+  },
+  async resolve(parent: any, args: any) {
+    let { content, title, thumbnail } = args;
+
+    await Shows.insert({
+      content,
+      title,
+      thumbnail,
+    });
+    return args;
+  },
+};
