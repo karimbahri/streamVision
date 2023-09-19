cd0d54a52fe1a1fcb8e079593475f055b1e2f710
GEEK1050 <karim-bahri-18@outlook.fr>
2023-09-19 21:29:58 +0100

    update shows type ( adding category seasons and episodes attributes)

--- server/src/schema/typedefs/shows.ts
+++ server/src/schema/typedefs/shows.ts
@@ -1,12 +1,20 @@
-import { GraphQLObjectType, GraphQLID, GraphQLString } from "graphql";
+import {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+} from "graphql";
 
 const ShowType = new GraphQLObjectType({
-  name: "User",
+  name: "Shows",
   fields: () => ({
     id: { type: GraphQLID },
     content: { type: GraphQLString },
     title: { type: GraphQLString },
     thumbnail: { type: GraphQLString },
+    category: { type: GraphQLString },
+    seasons: { type: GraphQLInt },
+    episodes: { type: GraphQLInt },
   }),
 });
 
