65bd3bd37c5d00c83b6976f6e2981227a37c9e52
GEEK1050 <karim-bahri-18@outlook.fr>
2023-09-23 10:42:03 +0100

    set login mutation

---
+++ client/src/graphQl/mutations.ts
@@ -0,0 +1,61 @@
+import gql from "graphql-tag";
+
+export const LOGIN_MUTATION = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password)
+  }
+`;
+
+export const SIGNUP_MUTATION = gql`
+  mutation createUser(
+    $userName: String!
+    $password: String!
+    $passwordConfirmation: String!
+    $fullName: String!
+    $email: String!
+    $birthday: String!
+  ) {
+    createUser(
+      userName: $userName
+      password: $password
+      passwordConfirmation: $passwordConfirmation
+      fullName: $fullName
+      email: $email
+      birthday: $birthday
+    ) {
+      id
+    }
+  }
+`;
+
+export const RESET_USER = gql`
+  mutation resetUser($email: String!) {
+    resetUser(email: $email) {
+      id
+    }
+  }
+`;
+
+export const RESET_PASSWORD = gql`
+  mutation updateUserPassword(
+    $email: String!
+    $code: String!
+    $newPassword: String!
+    $oldPassword: String!
+  ) {
+    updateUserPassword(
+      email: $email
+      code: $code
+      newPassword: $newPassword
+      oldPassword: $oldPassword
+    ) {
+      id
+    }
+  }
+`;
+
+export const CHECK_VERIFICATION_CODE = gql`
+  mutation chaeckVerificationUser($email: String!, $code: String!) {
+    chaeckVerificationUser(email: $email, code: $code)
+  }
+`;
