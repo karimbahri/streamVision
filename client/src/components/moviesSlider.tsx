a00679588c0650cdac30f08046aeffba054538de
GEEK1050 <karim-bahri-18@outlook.fr>
2023-09-26 18:01:13 +0100

    retrieve fetched data and set display

--- client/src/components/moviesSlider.tsx
+++ client/src/components/moviesSlider.tsx
@@ -3,7 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-export default function MoviesSlider() {
+export default function MoviesSlider(props: any) {
   var settings = {
     dots: false,
     infinite: false,
@@ -16,53 +16,42 @@ export default function MoviesSlider() {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3
-        }
+          slidesToScroll: 3,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
-        }
+          initialSlide: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
+
   return (
     <div className={"movies-sliders"}>
-      {/* <div className="sliders-box">
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-      </div>
-      <a className="arrow left-arrow" href="#">
-        {"<"}
-      </a>
-      <a className="arrow right-arrow" href="#">
-        {">"}
-      </a> */}
+      <h1 className={"movies-sliders__header"}>{props.section}</h1>
       <Slider {...settings}>
+        {props.data.getLatestAll.map((movie: any) => (
+          <Movie key={movie.id} />
+        ))}
+        {/* <Movie />
         <Movie />
         <Movie />
         <Movie />
         <Movie />
         <Movie />
         <Movie />
-        <Movie />
-        <Movie />
+        <Movie /> */}
       </Slider>
     </div>
   );
