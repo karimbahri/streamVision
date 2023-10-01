70bf4cef46ce10ab7b3e378601218a27c5ade0ec
GEEK1050 <karim-bahri-18@outlook.fr>
2023-10-01 12:55:07 +0100

    implement homepage loader

---
+++ client/src/components/homepageLoader.tsx
@@ -0,0 +1,15 @@
+export default function HomepageLoader() {
+  return (
+    <div className={"lds-grid-homepage"}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
