73ad0b2ec25d949364885ef20d8b5cb6dda67755
GEEK1050 <karim-bahri-18@outlook.fr>
2023-10-04 13:11:53 +0100

    clean code

--- client/src/components/streamPlayer.tsx
+++ client/src/components/streamPlayer.tsx
@@ -4,18 +4,10 @@ import { Player, BigPlayButton } from "video-react";
 export default function StreamPlayer(props: any) {
   return (
     <div className="stream-player">
-      <div className="video-wrapper">
-        <Player
-          // ref={(player) => {
-          //   this.player = player;
-          // }}
-          // fluid={false}
-          aspectRatio="16:9"
-        >
-          <BigPlayButton position="center" />
-          <source src={props.url} />
-        </Player>
-      </div>
+      <Player aspectRatio="16:9">
+        <BigPlayButton position="center" />
+        <source src={props.url} />
+      </Player>
     </div>
   );
 }
