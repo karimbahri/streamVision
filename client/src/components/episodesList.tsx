2c1a7a9c02a21b2e3c0a55990935805ab9a8c6d1
GEEK1050 <karim-bahri-18@outlook.fr>
2023-10-11 00:53:30 +0100

    set episode url

--- client/src/components/episodesList.tsx
+++ client/src/components/episodesList.tsx
@@ -1,44 +1,37 @@
-export default function EpisodesList() {
+import { Link, useLocation } from "react-router-dom";
+
+export default function EpisodesList(props: any) {
   const seasonSelector = (event: any) => {
     console.log(event.target.value);
   };
+  const { pathname, search } = useLocation();
+  const options = [];
+  const episodes = [];
 
+  for (let i = 1; i <= props.seasons; i++)
+    options.push(<option value={`season_${i}`}> {`Season ${i}`} </option>);
+
+  for (let j = 1; j <= props.episodes; j++)
+    episodes.push(
+      <li className="episode-list__item">
+        <Link
+          to={
+            search.includes("episode=")
+              ? `${pathname}${search}`.replace(/&episode=\d+/g, `&episode=${j}`)
+              : `${pathname}${search}&episode=${j}`
+          }
+        >{`Episode ${j}`}</Link>
+      </li>
+    );
   return (
     <div className="episode-list__wrapper">
-      {/* <h1 className="episode-list__header">Episodes</h1> */}
-      <select className="season-list" onChange={seasonSelector}>
-        <option value="season_1"> season 1 </option>
-        <option value="season_2"> season 2 </option>
-        <option value="season_3"> season 3 </option>
-        <option value="season_4"> season 4 </option>
-      </select>
+      {options.length && (
+        <select className="season-list" onChange={seasonSelector}>
+          {options}
+        </select>
+      )}
 
-      <ul className="episode-list">
-        <li className="episode-list__item">
-          <a href="#">Episode 1</a>
-        </li>
-        <li className="episode-list__item">
-          <a href="#">Episode 2</a>
-        </li>
-        <li className="episode-list__item">
-          <a href="#">Episode 3</a>
-        </li>
-        <li className="episode-list__item">
-          <a href="#">Episode 4</a>
-        </li>
-        <li className="episode-list__item">
-          <a href="#">Episode 5</a>
-        </li>
-        <li className="episode-list__item">
-          <a href="#">Episode 6</a>
-        </li>
-        <li className="episode-list__item">
-          <a href="#">Episode 7</a>
-        </li>
-        <li className="episode-list__item">
-          <a href="#">Episode 10</a>
-        </li>
-      </ul>
+      <ul className="episode-list">{episodes}</ul>
     </div>
   );
 }
