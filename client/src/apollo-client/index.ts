f674377a7837fa403f3261d63da9ed6cfe8aeb6e
GEEK1050 <karim-bahri-18@outlook.fr>
2023-11-13 18:41:59 +0100

    link client to hosted api

--- client/src/apollo-client/index.ts
+++ client/src/apollo-client/index.ts
@@ -28,7 +28,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 // });
 
 const client = new ApolloClient({
-  uri: "http://localhost:8080/graphql",
+  uri: import.meta.env.VITE_API_URL || "http://localhost:8080/graphql",
   cache: new InMemoryCache(),
 });
 
